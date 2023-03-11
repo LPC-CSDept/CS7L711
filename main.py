a9db817bbd4c597766b8c7d00be9470d618fadff
Kyu Lee <kwooler@gmail.com>
2023-03-11 14:30:38 -0800

    L711

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+rsum = []
+
+
 def main():
     numbers = [[11, 12, 13, 14, 15],
                [21, 22, 23, 24, 25],
                [31, 32, 33, 34, 35]]
-
-    rownum = int(input('Enter the row num: '))
+    global rsum
     ##################################################
     # Comlete your code here
     ##################################################
+    for i in range(len(numbers)):
+        total = 0
+        for j in range(len(numbers[i])):
+            total += numbers[i][j]
+        rsum.append(total)
+        # main.rsum.append(total)
+    print(rsum)
+    # print(main.rsum)
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -3,12 +3,14 @@ import io
 import sys
 import re
 
+# global rsum
+
 
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '2'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = '2'
+    # sys.stdin = io.StringIO(datastr)
 
     main.main()
     sys.stdout = sys.__stdout__
@@ -18,27 +20,13 @@ def test_main_1():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*165[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
-
-
-def test_main_2():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    datastr = '1'
-    sys.stdin = io.StringIO(datastr)
-
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # regex_string = r'[\w,\W]*165[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*115[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # assert res != None
+    # print(res.group())
+    # assert main.main.rsum[0] == 65
+    # assert main.main.rsum[1] == 115
+    # assert main.main.rsum[2] == 165
+    assert main.rsum[0] == 65
+    assert main.rsum[1] == 115
+    assert main.rsum[2] == 165
