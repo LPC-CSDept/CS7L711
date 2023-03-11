5f31cc0eae565561848c2b1bddcd30fedc8ef9f5
Kyu Lee <kwooler@gmail.com>
2023-03-11 14:32:20 -0800

    L711

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     numbers = [[11, 12, 13, 14, 15],
                [21, 22, 23, 24, 25],
                [31, 32, 33, 34, 35]]
-    global rsum
+    global rsum     # Use this global variable to save the summation of the row values
     ##################################################
     # Comlete your code here
     ##################################################
